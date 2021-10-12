769fa412976f41dbacb4af0a2fb588a5605761a3
obru <obrusetiawan@gmail.com>
2021-10-12 09:56:21 +0700

    add home

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'
+
+    class Homeview extends StatelessWidget {
+      const Homeview({Key? key}) : super(key: key);
+
+      @override
+      Widget build(BuildContext context) {
+        return Container(
+          child: Center(
+            child: Text("Home"),
+          ),
+        );
+
+      }
+    }
